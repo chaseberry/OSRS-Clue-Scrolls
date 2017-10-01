fc0b7529f890c58f4d4ccd4408ba0831fc23e9b0
Chase <chase@csh.rit.edu>
2017-10-01 18:53:28 -0400

    Work on emote clues

--- src/main/kotlin/edu/csh/chase/osrs/cluescrolls/solver/models/clues/EmoteClue.kt
+++ src/main/kotlin/edu/csh/chase/osrs/cluescrolls/solver/models/clues/EmoteClue.kt
@@ -2,10 +2,19 @@ package edu.csh.chase.osrs.cluescrolls.solver.models.clues
 
 import edu.csh.chase.osrs.cluescrolls.solver.models.Emotes
 import edu.csh.chase.osrs.cluescrolls.solver.models.Location
+import edu.csh.chase.osrs.cluescrolls.solver.models.loadout.Loadout
 
 class EmoteClue(level: Level,
                 location: Location,
-                val emote: Emotes) : ClueScroll(level, location) {
+                val loadout: Loadout,
+                val task: Task) : ClueScroll(level, location) {
+
+    sealed class Task(val emote: Emotes) {
+        class Emote(emote: Emotes) : Task(emote)
+        class DoubleAgent(emote: Emotes) : Task(emote)
+        class DoubleEmote(emote1: Emotes, val emote2: Emotes) : Task(emote1)
+    }
+
 }
 
 /*
