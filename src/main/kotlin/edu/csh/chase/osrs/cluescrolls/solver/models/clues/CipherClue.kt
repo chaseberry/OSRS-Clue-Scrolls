5d67a65ab5e8da0cecceb6c4caf5752c9341d136
Chase <chase@csh.rit.edu>
2017-10-01 18:59:43 -0400

    Added CipherClues

---
+++ src/main/kotlin/edu/csh/chase/osrs/cluescrolls/solver/models/clues/CipherClue.kt
@@ -0,0 +1,14 @@
+package edu.csh.chase.osrs.cluescrolls.solver.models.clues
+
+import edu.csh.chase.osrs.cluescrolls.solver.models.Challenge
+import edu.csh.chase.osrs.cluescrolls.solver.models.Location
+import edu.csh.chase.osrs.cluescrolls.solver.models.npcs.Npc
+
+class CipherClue(level: Level,
+                 location: Location,
+                 val cipher: String,
+                 val npc: Npc,
+                 val challenge: Challenge) : ClueScroll(level, location) {
+
+
+}
